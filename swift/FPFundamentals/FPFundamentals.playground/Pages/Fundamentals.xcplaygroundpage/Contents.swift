f554259573ea585547ace2c1eb1a4986f8a8e3ce
Tomás Ruiz-López <truizlop@gmail.com>
2019-01-21 13:51:51 +0100

    Step 5: Create typeclass Transformer2

--- swift/FPFundamentals/FPFundamentals.playground/Pages/Fundamentals.xcplaygroundpage/Contents.swift
+++ swift/FPFundamentals/FPFundamentals.playground/Pages/Fundamentals.xcplaygroundpage/Contents.swift
@@ -66,6 +66,20 @@ protocol Transformer {
     func map<A, B>(_ fa: Kind<F, A>, _ f: @escaping (A) -> B) -> Kind<F, B>
 }
 
+protocol Transformer2: Transformer {
+    func ap<A, B>(_ fa: Kind<F, A>, _ ff: Kind<F, (A) -> B>) -> Kind<F, B>
+}
+
+extension Transformer2 {
+    func product<A, B>(_ fa: Kind<F, A>, _ fb: Kind<F, B>) -> Kind<F, (A, B)> {
+        return ap(fb, map(fa, { a in { b in (a, b) } }))
+    }
+    
+    func map2<A, B, Z>(_ fa: Kind<F, A>, _ fb: Kind<F, B>, _ f: @escaping (A, B) -> Z) -> Kind<F, Z> {
+        return map(product(fa, fb), f)
+    }
+}
+
 // Instances
 
 class IntCombinator: Combinator {
@@ -111,6 +125,15 @@ class MaybeTransformer: Transformer {
     }
 }
 
+class MaybeTransformer2: MaybeTransformer, Transformer2 {
+    func ap<A, B>(_ fa: Kind<ForMaybe, A>, _ ff: Kind<ForMaybe, (A) -> B>) -> Kind<ForMaybe, B> {
+        return ff.fix().fold({ Maybe<B>.no() },
+                             { f in fa.fix().fold({ Maybe<B>.no() },
+                                                  { a in Maybe.yes(f(a)) })
+        })
+    }
+}
+
 extension Maybe {
     static func combinator<CV>(_ cv: CV) -> MaybeCombinator<A, CV> where CV: Combinator, CV.A == A {
         return MaybeCombinator(cv)
@@ -118,6 +141,10 @@ extension Maybe {
     
     static func transformer() -> MaybeTransformer {
         return MaybeTransformer()
+    }
+    
+    static func transformer2() -> MaybeTransformer2 {
+        return MaybeTransformer2()
     }
 }
 
@@ -128,19 +155,33 @@ struct Account {
     let balance: Int
 }
 
+struct Statement {
+    let isRich: Bool
+    let accounts: Int
+}
+
 class Program {
     let getBalanceBank1: Maybe<Account> = .yes(Account(id: "a1", balance: 100))
     let getBalanceBank2: Maybe<Int> = .yes(80)
     
+    var b1: Maybe<Int> {
+        return Maybe<Any>.transformer().map(getBalanceBank1, { acc in acc.balance }).fix()
+    }
+    
+    var b2: Maybe<Int> { return self.getBalanceBank2 }
+    
     var balance: Maybe<Int> {
-        let b1: Maybe<Int> = Maybe<Any>.transformer().map(getBalanceBank1, { acc in acc.balance }).fix()
-        let b2: Maybe<Int> = getBalanceBank2
-        
         return Maybe.combinator(Int.combinator).combine(b1, b2)
+    }
+    
+    var statement: Maybe<Statement> {
+        return Maybe<Any>.transformer2().map2(b1, b2) { x, y in
+            Statement(isRich: (x + y > 1000), accounts: 2) }.fix()
     }
     
     func run() {
         print(balance)
+        print(statement)
     }
 }
 
