1c5aaf6557876e65d8875bdce0c4dc748d1bf29c
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-01-25 17:50:56 +0100

    Kotlin step 1a: Create data type Maybe

--- kotlin/src/main/kotlin/com/fortyseven/fpfundamentals/Contents.kt
+++ kotlin/src/main/kotlin/com/fortyseven/fpfundamentals/Contents.kt
@@ -1,10 +1,19 @@
 package com.fortyseven.fpfundamentals
 
+// Data types
+
+sealed class Maybe<A> {
+  data class Present<A>(val a: A) : Maybe<A>()
+  object Absent : Maybe<Nothing>()
+}
+
+// Program
+
 class Program {
 
-  fun getBalanceBank1(): Int = 100
+  fun getBalanceBank1(): Maybe<Int> = Maybe.Present(100)
 
-  fun getBalanceBank2(): Int = 80
+  fun getBalanceBank2(): Maybe<Int> = Maybe.Present(80)
 
   val balance: Int
     get() = getBalanceBank1() + getBalanceBank2()
